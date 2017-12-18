9dfcb5a80593ecb89f16e5f42f19f278b35131df
Sean Spearman <sean.spearman@procore.com>
2017-12-17 18:06:00 -0800

    Fix path for deploying to github pages

--- sw.js
+++ sw.js
@@ -1,28 +1,35 @@
-self.addEventListener('install', function(e) {
+const ENV = 'production';
+let rootPrefix = '';
+if (ENV === 'production') {
+  rootPrefix = 'teatime';
+}
+
+self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('teatime').then(function(cache) {
+    caches.open('teatime').then((cache) => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/teatime.css',
-        '/teatime.js',
-        '/audio/boiling_water_long.ogg',
-        '/audio/tim_tum.ogg',
-        '/images/cup_st.png',
-        '/images/cup_front.png',
-        '/images/cup_back.png',
-        '/images/arches.png',
-        '/images/arches_red.png',
-        '/images/uncheck.png',
-        '/images/check.png',
+        `${rootPrefix}/`,
+        `${rootPrefix}/sw.js`,
+        `${rootPrefix}/index.html`,
+        `${rootPrefix}/teatime.css`,
+        `${rootPrefix}/teatime.js`,
+        `${rootPrefix}/audio/boiling_water_long.ogg`,
+        `${rootPrefix}/audio/tim_tum.ogg`,
+        `${rootPrefix}/images/cup_st.png`,
+        `${rootPrefix}/images/cup_front.png`,
+        `${rootPrefix}/images/cup_back.png`,
+        `${rootPrefix}/images/arches.png`,
+        `${rootPrefix}/images/arches_red.png`,
+        `${rootPrefix}/images/uncheck.png`,
+        `${rootPrefix}/images/check.png`,
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then((response) => {
       if (response === undefined) { console.log('could not find ', event.request.url); }
       return response || fetch(event.request);
     })
