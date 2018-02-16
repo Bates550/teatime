3bae42500ebb0fb87464fb26f33c2390fce39cb0
Sean Spearman <sean.spearman@procore.com>
2018-02-15 23:00:21 -0800

    Run create-react-app and style basic layout

---
+++ react-teatime/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="appWrapper">
+        <div className="header" />
+        <div className="body" />
+        <div className="footer" />
+      </div>
+    );
+  }
+}
+
+export default App;
